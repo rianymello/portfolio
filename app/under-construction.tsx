86b4a7078704cb8021322c5c846df04fadc34ac2
Riany Mello <mello.riany@gmail.com>
2025-03-04 19:12:00 +0000

    update

---
+++ app/under-construction.tsx
@@ -0,0 +1,8 @@
+export default function UnderConstrutionPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen text-center">
+            <h1 className="text-4xl font-bold">🚧 Página em Construção 🚧</h1>
+            <p className="text-lg mt-4">Estamos trabalhando para trazer essa funcionalidade em breve!</p>
+        </div>
+    );
+}
